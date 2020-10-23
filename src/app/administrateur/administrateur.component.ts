5467cf35cc5b86c74f1a811b17d5b6b618e37265
fokou87 <ghislain.fokou@digiplusconsulting.com>
2020-10-23 15:43:14 +0100

    23/10/2020

--- src/app/administrateur/administrateur.component.ts
+++ src/app/administrateur/administrateur.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from '../Service/LoginService';
+import {Filliere} from '../Model/Filliere';
+import {Module_Ec} from '../Model/Module_Ec';
+import {EcModule} from '../Model/EcModule';
 
 @Component({
   selector: 'app-administrateur',
@@ -9,8 +12,11 @@ import {LoginService} from '../Service/LoginService';
 export class AdministrateurComponent implements OnInit {
 
   navbar_filliere:number=1;
+  choi_ec:number=0;
 
-
+  public fillier:Filliere;
+  public module_ec:Module_Ec;
+  public element_constitu:EcModule;
   constructor(public loginService:LoginService) { }
 
   ngOnInit(): void {
@@ -20,4 +26,11 @@ export class AdministrateurComponent implements OnInit {
     this.navbar_filliere=n;
   }
 
+  choix_ec_admin(n:number){
+    this.choi_ec=n;
+  }
+
+  select_filier($event: MouseEvent) {
+
+  }
 }
